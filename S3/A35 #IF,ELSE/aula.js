03298170ef2451c9e8a377aedc296b3f8b615b22
Henrique Fonseca <henriquefons@hotmail.com>
2020-12-28 22:21:31 -0300

    Adição do modelo exercicio sessao 3

---
+++ S3/A35 #IF,ELSE/aula.js
@@ -0,0 +1,19 @@
+// Se achar uma condicao verdadeira, o codigo para e apresanta e resposta
+
+function hora(n) {
+  if (n >= 7 && n < 12) {
+    return "Bom dia";
+  } else if (n >= 12 && n < 18) {
+    return "Boa tarde";
+  } else {
+    return "Boa noite";
+  }
+}
+
+console.log(hora(18));
+
+const tenhoGrana = (n) => {
+  return n && "Vou sair de casa";
+};
+
+console.log(tenhoGrana(true));
